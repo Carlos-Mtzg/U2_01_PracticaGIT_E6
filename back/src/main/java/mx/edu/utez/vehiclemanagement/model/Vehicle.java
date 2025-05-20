28a42159fa5a6e861e9d8f0498413d3b5b5a133e
Leo-Noriega <leonoriega100@gmail.com>
2025-05-20 16:41:49 -0600

    feat(vehicle): add crud logic from vehicle

--- back/src/main/java/mx/edu/utez/vehiclemanagement/model/Vehicle.java
+++ back/src/main/java/mx/edu/utez/vehiclemanagement/model/Vehicle.java
@@ -1,4 +1,31 @@
 package mx.edu.utez.vehiclemanagement.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "vehiculos")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vehicle {
+    @Id
+    @Column
+    private int vehicleId;
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private String modelo;
+
+    @Column(nullable = false)
+    private String marca;
+
+    // TODO: Eliminar comentario dependiendo de la relación de proveedor
+//    @ManyToOne
+//    @JoinColumn(name = "proveedor_id", nullable = false)
+//    private Proveedor proveedor;
 }
