bd4f2241964ced67869ddee784bd47885f1452c1
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-05-20 22:13:44 -0600

    feat: implement layout structure with header, footer, and routing for vehicles and suppliers

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './global.css'
+import Layout from './pages/Layout/Layout';
+import Suppliers from './pages/Home/Suppliers';
+import Vehicles from './pages/Home/Vehicles';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/vehicles',
+        element: <Vehicles />,
+        errorElement: ''
+      },
+      {
+        path: '/suppliers',
+        element: <Suppliers />,
+        errorElement: ''
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
