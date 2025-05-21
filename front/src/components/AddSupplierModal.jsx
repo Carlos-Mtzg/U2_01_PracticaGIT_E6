04985ef440e9d1489fe60d5459ee61bc701b8d9d
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-05-21 01:23:18 -0600

    feat: add AddSupplierModal and UpdateSupplierModal components; implement supplier management in Suppliers page

---
+++ front/src/components/AddSupplierModal.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import { registerSupplier } from '../Services/ApiSuppliers'
+
+const AddSupplierModal = ({ onClose, onRegister }) => {
+    const [nombre, setNombre] = useState('')
+    const [correo, setCorreo] = useState('')
+    const [telefono, setTelefono] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await registerSupplier({ nombre, correo, telefono })
+            onRegister()
+            onClose()
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 max-h-full bg-black/40">
+            <div className="relative p-4 w-full max-w-2xl max-h-full">
+                <div className="relative bg-white rounded-lg shadow-sm">
+                    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t bg-blue-950">
+                        <h3 className="text-xl font-semibold text-white">
+                            Registrar Proveedor
+                        </h3>
+                    </div>
+                    <form className="p-4 md:p-5 space-y-4" onSubmit={handleSubmit}>
+                        <label htmlFor="name">Nombre:</label>
+                        <input
+                            type="text"
+                            name='name'
+                            className="w-full border rounded px-3 py-2"
+                            placeholder="Nombre"
+                            value={nombre}
+                            onChange={e => setNombre(e.target.value)}
+                            required
+                        />
+                        <label htmlFor="email">Correo Electrónico:</label>
+                        <input
+                            type="email"
+                            name='email'
+                            className="w-full border rounded px-3 py-2"
+                            placeholder="Correo"
+                            value={correo}
+                            onChange={e => setCorreo(e.target.value)}
+                            required
+                        />
+                        <label htmlFor="phone">Teléfono:</label>
+                        <input
+                            type="text"
+                            name='phone'
+                            className="w-full border rounded px-3 py-2"
+                            placeholder="Teléfono"
+                            value={telefono}
+                            onChange={e => setTelefono(e.target.value)}
+                            required
+                        />
+                        <div className="flex gap-4 justify-end pt-4">
+                            <button type="button" className="px-6 py-2 cursor-pointer text-white bg-gray-300 rounded-full" onClick={onClose}>
+                                Cancelar
+                            </button>
+                            <button type="submit" className="px-6 py-2 cursor-pointer text-white bg-blue-950 rounded-full">
+                                Confirmar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddSupplierModal
--- front/src/pages/Layout/Header.jsx
+++ front/src/pages/Layout/Header.jsx
@@ -4,7 +4,7 @@ import logo from '@assets/images/logo.png'
 
 const Header = () => {
     return (
-        <header className='flex justify-between items-center pr-8 pl-8 bg-blue-950/90 h-18 rounded-full mx-4 mt-4'>
+        <header className='flex justify-between items-center pr-8 pl-8 bg-blue-950/90 h-18'>
             <div className='flex items-center gap-5'>
                 <img className='h-18' src={logo} alt="Logo de la pagina" />
                 <h1 className='text-3xl uppercase font-bold text-white border-l-4 pl-4 border-white'>Vehicles Manager</h1>
