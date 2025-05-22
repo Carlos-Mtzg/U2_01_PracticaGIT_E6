ba1f4bd8a3d91952958fb374e11af72f7a385cb3
Leo-Noriega <leonoriega100@gmail.com>
2025-05-22 10:37:22 -0600

    feat: add dto for vehicles

---
+++ back/src/main/java/mx/edu/utez/vehiclemanagement/controller/VehicleDto.java
@@ -0,0 +1,14 @@
+package mx.edu.utez.vehiclemanagement.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VehicleDto {
+    private String color;
+    private String modelo;
+    private String marca;
+    private int provedorId;
+
+}
